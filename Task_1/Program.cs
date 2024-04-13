992fe150c173eb5e64527f25e08588eda4030c67
Darya Kauzova <amayasuzuki12@gmail.com>
2024-04-13 21:00:33 +0300

    Created first task

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[5];
+arr[0] = 4;
+arr[1] = 19;
+arr[2] = 7;
+arr[3] = 2;
+arr[4] = 8;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 1, 83, 9, 493, 3 };
+
+
+int[] arr3 = {1, 5, ,35, 5437, 2};
